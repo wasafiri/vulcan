3f7ad1990bb59ea33bdc4d7e314fa6088f7b3604
DAVID BAHAR <dbahar@gwu.edu>
2024-12-26 21:37:29 -0500

    work on making tests pass

--- test/controllers/admin/products_controller_test.rb
+++ test/controllers/admin/products_controller_test.rb
@@ -1,43 +1,51 @@
 require "test_helper"
 
-describe Admin::ProductsController do
-  it "gets index" do
-    get admin_products_index_url
-    must_respond_with :success
-  end
+class Admin::ProductsControllerTest < ActionDispatch::IntegrationTest
+ setup do
+   @product = products(:one)
+   @admin = users(:admin)
+   sign_in_as(@admin)
+ end
 
-  it "gets show" do
-    get admin_products_show_url
-    must_respond_with :success
-  end
+ test "should get index" do
+   get admin_products_path
+   assert_response :success
+ end
 
-  it "gets new" do
-    get admin_products_new_url
-    must_respond_with :success
-  end
+ test "should get show" do
+   get admin_product_path(@product)
+   assert_response :success
+ end
 
-  it "gets create" do
-    get admin_products_create_url
-    must_respond_with :success
-  end
+ test "should get new" do
+   get new_admin_product_path
+   assert_response :success
+ end
 
-  it "gets edit" do
-    get admin_products_edit_url
-    must_respond_with :success
-  end
+ test "should create product" do
+   assert_difference("Product.count") do
+     post admin_products_path, params: { product: { name: "Test Product" } }
+   end
+   assert_redirected_to admin_product_path(Product.last)
+ end
 
-  it "gets update" do
-    get admin_products_update_url
-    must_respond_with :success
-  end
+ test "should get edit" do
+   get edit_admin_product_path(@product)
+   assert_response :success
+ end
 
-  it "gets archive" do
-    get admin_products_archive_url
-    must_respond_with :success
-  end
+ test "should update product" do
+   patch admin_product_path(@product), params: { product: { name: "Updated" } }
+   assert_redirected_to admin_product_path(@product)
+ end
 
-  it "gets unarchive" do
-    get admin_products_unarchive_url
-    must_respond_with :success
-  end
+ test "should archive product" do
+   patch archive_admin_product_path(@product)
+   assert_redirected_to admin_products_path
+ end
+
+ test "should unarchive product" do
+   patch unarchive_admin_product_path(@product)
+   assert_redirected_to admin_products_path
+ end
 end
