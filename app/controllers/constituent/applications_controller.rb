7ae8d060d852ca307eb7e987c7441a212e3d8b87
DAVID BAHAR <dbahar@gwu.edu>
2024-12-29 11:02:04 -0500

    bundle update

--- app/controllers/constituent/applications_controller.rb
+++ app/controllers/constituent/applications_controller.rb
@@ -6,10 +6,25 @@ class Constituent::ApplicationsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @application.update(application_params)
+      redirect_to constituent_application_path(@application), notice: "Application updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_application
     @application = current_user.applications.find(params[:id])
+  end
+
+  def application_params
+    params.require(:application).permit(:status, :application_type, :household_size, :annual_income, :residency_details, :medical_provider_id)
   end
 
   def require_constituent!
