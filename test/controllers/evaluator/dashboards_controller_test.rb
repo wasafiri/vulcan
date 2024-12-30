618c48e90b2c211f35396739a47bcd45c5f1c695
DAVID BAHAR <dbahar@gwu.edu>
2024-12-30 12:37:03 -0500

    pluralize other controller refs in tests

--- test/controllers/evaluator/dashboards_controller_test.rb
+++ test/controllers/evaluator/dashboards_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-describe Evaluator::DashboardController do
+describe Evaluator::DashboardsController do
   it "gets show" do
     get evaluator_dashboard_show_url
     must_respond_with :success
--- test/controllers/vendor/dashboards_controller_test.rb
+++ test/controllers/vendor/dashboards_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-describe Vendor::DashboardController do
+describe Vendor::DashboardsController do
   it "gets show" do
     get vendor_dashboard_show_url
     must_respond_with :success
