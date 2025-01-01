64fb14f6286109042d65852882922cdb41522231
DAVID BAHAR <dbahar@gwu.edu>
2025-01-01 12:19:47 -0500

    modify fields and update controller to get application page showing

--- app/controllers/constituent/applications_controller.rb
+++ app/controllers/constituent/applications_controller.rb
@@ -3,6 +3,25 @@ class Constituent::ApplicationsController < ApplicationController
   before_action :require_constituent!
   before_action :set_application, only: [ :show, :edit, :update ]
 
+  def new
+    @application = current_user.applications.new
+  end
+
+  def create
+    @application = current_user.applications.new(application_params)
+    @application.application_date = Time.current
+    @application.status = :in_progress
+
+    ActiveRecord::Base.transaction do
+      if current_user.update(disability_params) && @application.save
+        redirect_to constituent_application_path(@application), notice: "Application submitted successfully."
+      else
+        @application.errors.merge!(current_user.errors)
+        render :new, status: :unprocessable_entity
+      end
+    end
+  end
+
   def show
   end
 
@@ -24,7 +43,27 @@ class Constituent::ApplicationsController < ApplicationController
   end
 
   def application_params
-    params.require(:application).permit(:status, :application_type, :household_size, :annual_income, :residency_details, :medical_provider_id)
+    params.require(:application).permit(
+      :household_size,
+      :annual_income,
+      :residency_details,
+      :self_certify_disability,
+      :medical_provider_id
+    )
+  end
+
+  def disability_params
+    params.require(:application).permit(
+      :is_guardian,
+      :guardian_relationship,
+      :residency_proof,
+      :income_proof,
+      :hearing_disability,
+      :vision_disability,
+      :speech_disability,
+      :mobility_disability,
+      :cognition_disability
+    )
   end
 
   def require_constituent!
