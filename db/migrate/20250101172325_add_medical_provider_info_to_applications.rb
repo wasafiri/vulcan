e91ad1a943ad3168d48e929cb6af2dd4bfe21a4e
DAVID BAHAR <dbahar@gwu.edu>
2025-01-01 12:29:28 -0500

    resolve errors with application, fix database tables

---
+++ db/migrate/20250101172325_add_medical_provider_info_to_applications.rb
@@ -0,0 +1,8 @@
+class AddMedicalProviderInfoToApplications < ActiveRecord::Migration[8.0]
+  def change
+    add_column :applications, :medical_provider_name, :string
+    add_column :applications, :medical_provider_phone, :string
+    add_column :applications, :medical_provider_fax, :string
+    add_column :applications, :medical_provider_email, :string
+  end
+end
