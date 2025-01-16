04c360879cf798f8228c39962e065f48e2e86119
DAVID BAHAR <dbahar@gwu.edu>
2025-01-16 14:37:43 -0500

    fix a few tests still using rspec methods, switch to minitest

--- test/controllers/evaluator/evaluations_controller_test.rb
+++ test/controllers/evaluator/evaluations_controller_test.rb
@@ -1,53 +1,53 @@
 require "test_helper"
 
-describe Evaluator::EvaluationsController do
-  it "gets index" do
+class Evaluator::EvaluationsControllerTest < ActionDispatch::IntegrationTest
+  test "gets index" do
     get evaluator_evaluations_index_url
-    must_respond_with :success
+    assert_response :success
   end
 
-  it "gets show" do
+  test "gets show" do
     get evaluator_evaluations_show_url
-    must_respond_with :success
+    assert_response :success
   end
 
-  it "gets new" do
+  test "gets new" do
     get evaluator_evaluations_new_url
-    must_respond_with :success
+    assert_response :success
   end
 
-  it "gets create" do
+  test "gets create" do
     get evaluator_evaluations_create_url
-    must_respond_with :success
+    assert_response :success
   end
 
-  it "gets edit" do
+  test "gets edit" do
     get evaluator_evaluations_edit_url
-    must_respond_with :success
+    assert_response :success
   end
 
-  it "gets update" do
+  test "gets update" do
     get evaluator_evaluations_update_url
-    must_respond_with :success
+    assert_response :success
   end
 
-  it "gets submit_request" do
+  test "gets submit_request" do
     get evaluator_evaluations_submit_request_url
-    must_respond_with :success
+    assert_response :success
   end
 
-  it "gets request_additional_info" do
+  test "gets request_additional_info" do
     get evaluator_evaluations_request_additional_info_url
-    must_respond_with :success
+    assert_response :success
   end
 
-  it "gets pending" do
+  test "gets pending" do
     get evaluator_evaluations_pending_url
-    must_respond_with :success
+    assert_response :success
   end
 
-  it "gets completed" do
+  test "gets completed" do
     get evaluator_evaluations_completed_url
-    must_respond_with :success
+    assert_response :success
   end
 end
