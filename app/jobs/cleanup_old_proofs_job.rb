d9d76903e58b24b2ac2437bd70d5ad5a7b41f702
DAVID BAHAR <dbahar@gwu.edu>
2025-01-16 14:28:39 -0500

    fix class name in cleanup old proofs job

--- app/jobs/cleanup_old_proofs_job.rb
+++ app/jobs/cleanup_old_proofs_job.rb
@@ -1,8 +1,10 @@
-class CleanupProofsJob < ApplicationJob
+class CleanupOldProofsJob < ApplicationJob
+  queue_as :default
+
   def perform
     Application.where(status: :archived)
-              .where("updated_at < ?", 30.days.ago)
-              .find_each do |application|
+      .where("updated_at < ?", 30.days.ago)
+      .find_each do |application|
         application.purge_proofs
       end
   end
