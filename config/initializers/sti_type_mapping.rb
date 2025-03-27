6b21d0601e358160f46d25d76527acad1d81686e
DAVID BAHAR <dbahar@gwu.edu>
2025-03-27 14:51:45 -0400

    fix error in STI type mapping initializer

--- config/initializers/sti_type_mapping.rb
+++ config/initializers/sti_type_mapping.rb
@@ -6,7 +6,7 @@
 
 ActiveSupport.on_load(:active_record) do
   ActiveRecord::Base.singleton_class.class_eval do
-    alias_method original_find_sti_class find_sti_class
+    alias_method :original_find_sti_class, :find_sti_class
 
     def find_sti_class(type_name)
       if type_name == "Admin" && self <= User
